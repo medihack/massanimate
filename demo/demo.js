ab2367cbb3926b2a7eacf2c410020f612042cca9
Kai Schlamp <schlamp@gmx.de>
2011-03-31 12:02:37 +0200

    Improved the demo a bit.

--- demo/demo.js
+++ demo/demo.js
@@ -8,11 +8,14 @@ $(document).ready(function() {
 	$('<div id="start">Click Me!</div>').appendTo($("body")).click(function() {
 		var $this = $(this);
 		$this.hide();
-		massanimate.animate({width: 100, height: 100, opacity: 1}, 1500)
+		massanimate
+        .css({display: "block"})
+        .animate({width: 100, height: 100, opacity: 1}, 1500)
 		.animate({width: 50, height: 50, opacity: 0}, 1500, function() {
 			$this.fadeIn();
             $('<div id="comparilla">Comparilla.com</div>').appendTo($('body'))
             .hide().fadeIn(1000);
-		});
+            massanimate.css({display: "none"})
+		})
 	});
 });
